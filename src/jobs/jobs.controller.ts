ae33e08d5f43b461c1ba76fbfece307f3dccb01e
Japheth Jonathan <japheth.jonathan@synergyy.io>
2024-08-09 23:35:39 +0100

    Update

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -2,10 +2,13 @@ import { Controller, Get } from '@nestjs/common';
 import { JobsService } from './jobs.service';
 import { Job } from './jobs.entity';
 
+// Controller for handling requests related to jobs
 @Controller('jobs')
 export class JobsController {
+  // Inject JobsService to interact with job data
   constructor(private readonly jobsService: JobsService) {}
 
+  // Endpoint to get all jobs
   @Get()
   async getAllJobs(): Promise<Job[]> {
     return this.jobsService.getAllJobs();

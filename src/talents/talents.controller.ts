01b0b4c8660d3fc70f6d72ad3dea43562157a4b9
Japheth Jonathan <japheth.jonathan@synergyy.io>
2024-08-09 23:31:29 +0100

    Update

--- src/talents/talents.controller.ts
+++ src/talents/talents.controller.ts
@@ -2,35 +2,43 @@ import { Controller, Get } from '@nestjs/common';
 import { TalentsService } from './talents.service';
 import { TalentResponseDto, TalentStatsDto } from './dto/talent-stats.dto';
 
+// Controller for handling requests related to talents
 @Controller('dashboard')
 export class TalentsController {
+  // Inject TalentsService to interact with talent data
   constructor(private readonly talentsService: TalentsService) {}
 
+  // Endpoint to get the total number of talents
   @Get('total-talents')
   async getTotalNumberOfTalents() {
     return this.talentsService.getTotalNumberOfTalents();
   }
 
+  // Endpoint to get the number of talents per week
   @Get('talents-per-week')
   async getTalentsPerWeek() {
     return this.talentsService.getTalentsPerWeek();
   }
 
+  // Endpoint to get the number of talents per day
   @Get('talents-per-day')
   async getTalentsPerDay() {
     return this.talentsService.getTalentsPerDay();
   }
 
+  // Endpoint to get the number of verified talents
   @Get('verified-talents')
   async getVerifiedTalents() {
     return this.talentsService.getVerifiedTalents();
   }
 
+  // Endpoint to get talent statistics
   @Get('talents-stats')
   async getTalentsStats(): Promise<TalentStatsDto[]> {
     return this.talentsService.getTalentsStats();
   }
 
+  // Endpoint to get all talents
   @Get('all-talents')
   async getAllTalents(): Promise<TalentResponseDto[]> {
     return this.talentsService.getAllTalents();
